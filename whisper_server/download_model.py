87c123acd606c3cc56e99e2fd95be0c0906978c1
DUWIPANGGA <aryad2232@gmail.com>
2026-07-01 01:59:38 +0700

    remove temp download_model.py script

--- whisper_server/download_model.py
+++
@@ -1,8 +0,0 @@
-from faster_whisper.utils import download_model
-import os
-
-model_size = os.environ.get("WHISPER_MODEL_SIZE", "medium")
-output_dir = os.environ.get("MODEL_DIR", "/var/www/bps/whisper_server/models")
-print(f"Downloading {model_size} model to {output_dir}...")
-download_model(model_size, output_dir=output_dir)
-print("Done!")
