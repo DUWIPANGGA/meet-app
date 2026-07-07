fac3e095c51daaace4d558b963cb0fc5f96d3036
DUWIPANGGA <aryad2232@gmail.com>
2026-07-07 21:43:52 +0700

    hide edit button on arsip index

--- resources/views/admin/arsips/index.blade.php
+++ resources/views/admin/arsips/index.blade.php
@@ -49,7 +49,7 @@
                             <a href="{{ route('admin.arsips.show', $arsip) }}" class="p-2 rounded-lg hover:bg-[var(--nav-link-hover)] transition" title="Detail">
                                 <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" style="color:#6366f1"><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/><path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/></svg>
                             </a>
-                            <a href="{{ route('admin.arsips.edit', $arsip) }}" class="p-2 rounded-lg hover:bg-[var(--nav-link-hover)] transition" title="Edit">
+                            <a href="{{ route('admin.arsips.edit', $arsip) }}" class="p-2 rounded-lg hover:bg-[var(--nav-link-hover)] transition" title="Edit" style="display:none">
                                 <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" style="color:#f59e0b"><path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/></svg>
                             </a>
                             <form action="{{ route('admin.arsips.destroy', $arsip) }}" method="POST" onsubmit="return confirm('Yakin ingin menghapus arsip ini?');" class="inline">
